36188166b7c4b8eee91b408829e1d3e6e6ca6a7d
erickguerra22 <erickguerra2201@gmail.com>
2025-10-30 23:29:44 -0600

    fix: Funcionamiento en segundo plano con pm2

--- apiServices/document/document.controller.js
+++ apiServices/document/document.controller.js
@@ -82,7 +82,7 @@ export const uploadDocument = async (req, res) => {
             await sendEmail({
               to: email,
               subject: 'Error al procesar documento',
-              html: `<p>El documento <b>${docname}</b> no pudo procesarse ni subirse al servidor correctamente ${errorOutput}.</p>`,
+              html: `<p>El documento <b>${docname}</b> no pudo procesarse ni subirse al servidor correctamente. ${errorOutput}</p>`,
             })
             return
           }
@@ -105,7 +105,6 @@ export const uploadDocument = async (req, res) => {
                 html: `<p>El documento <b>${docname}</b> fue indexado exitosamente en el sistema.</p>`,
               })
             } else {
-              logger.error('El documento no se procesó correctamente', { title: 'Error en procesamiento de documento' })
               await sendEmail({
                 to: email,
                 subject: 'Error al procesar documento',
